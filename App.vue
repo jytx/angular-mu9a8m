e7828616356e23cdfd6a85d35c8a5cebfbddb77e
jytx <477939838@qq.com>
2022-07-30 17:49:13 +0800

    Initial commit

---
+++ App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref, h } from 'vue';
+
+/**
+ * Implement a functional component :
+ * 1. Render the list elements (ul/li) with the list data
+ * 2. Change the list item text color to red when clicked.
+ */
+const ListComponent = (props: any) => {
+  h('ul', [
+    ...props.list.map((item: { name: string }, index: number) =>
+      h(
+        'li',
+        {
+          style: { color: props['active-index'] === index ? 'red' : 'black' },
+          onClick: () => props.onToggle(index),
+        },
+        item.name
+      )
+    ),
+  ]);
+};
+
+const list = [
+  {
+    name: 'John',
+  },
+  {
+    name: 'Doe',
+  },
+  {
+    name: 'Smith',
+  },
+];
+
+const activeIndex = ref(0);
+
+function toggle(index: number) {
+  activeIndex.value = index;
+}
+</script>
+
+<template>
+  <list-component :list="list" :active-index="activeIndex" @toggle="toggle" />
+</template>
---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mu9a8m-xyin6l
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mu9a8m-xyin6l)
